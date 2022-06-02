24f4ccfd3c34fe295060ff978e890c0fd0b3b2b2
Rishi N Desai <rishindesai@yahoo.com>
2022-06-02 17:05:37 +0530

    files for footer and header components

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from "react";
+import "./styles/Header.css";
+
+const Header: React.FC = () => {
+  const [state, setState] = useState<string>();
+
+  return (
+    <h1>Hi</h1>
+  );
+}
+
+export default Header;
