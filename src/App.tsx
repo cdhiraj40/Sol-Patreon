98e224f77312d7cbbc3bb1a4b3f01a491f4efdaf
RishiDesai17 <rishindesai@yahoo.com>
2022-06-05 20:30:23 +0530

    fixed missing import

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Explore from "./pages/Explore";
-import Profile from './pages/Profile';
 import EditProfile from './pages/EditProfile';
 import './App.css';
 import Navbar from './components/Navbar';
