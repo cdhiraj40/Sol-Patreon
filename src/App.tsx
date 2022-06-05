6b0b00700e60e0b193858a5ee018017126d85354
RishiDesai17 <rishindesai@yahoo.com>
2022-06-05 19:49:26 +0530

    edit profile UI and basic validation

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Explore from "./pages/Explore";
+import Profile from './pages/Profile';
+import EditProfile from './pages/EditProfile';
 import './App.css';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
@@ -15,6 +17,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/explore" element={<Explore />} />
+          <Route path="/edit-profile/:walletaddress" element={<EditProfile />} />
         </Routes>
         <Footer/>
       </BrowserRouter>
