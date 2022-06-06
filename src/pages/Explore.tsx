7a60b514597ccab4b05df61acc90f0ff6b049656
cdhiraj40 <chauhandhiraj40@gmail.com>
2022-06-06 15:57:24 +0530

    updated profiles with blockchain data

--- src/pages/Explore.tsx
+++ src/pages/Explore.tsx
@@ -1,51 +1,75 @@
-import React, { useState } from "react";
+import React, {useState} from "react";
 import Grid from '@mui/material/Grid';
 import Button from '@mui/material/Button';
-import Footer from "../components/Footer";
 import "./styles/Explore.css";
+import {Profile} from "../api/Profile";
+import {FetchProfiles} from "../api/fetchProfile";
 
 const Explore: React.FC = () => {
-  const [state, setState] = useState<string>();
+    const [users, setUsers] = useState<Profile[]>([]);
+    const [load, setLoad] = useState(false);
+    const [count, setCount] = useState(0);
 
-  return (
-    <div>
-      <h1>Explore</h1>
+    async function fetchUsers() {
+        FetchProfiles()
+            .then((fetchProfiles: any) => {
+                setUsers(fetchProfiles)
+                console.log(fetchProfiles)
+            })
+            .finally(() => {
+            })
+    }
 
-      <div>
-        <div className="bg-image-container">
-          <img src="./images/explore page background.jpg" className="bg-image" />
-        </div>
-        <div className="bg-text">
-          <h1>Search bar</h1>
-        </div>
-      </div>
-      
-      <div className="flex-row">
-        <Grid container alignItems="center" style={{ width: "90%" }}>
-          {Array(6).fill(0).map(() => (
-            <Grid item md={4} sm={6} xs={12}>
-              <div className="card">
-                <Grid container alignItems="center">
-                  <Grid item lg={6} xs={12}>
-                    <div>
-                      <img src="./images/sample profile pic.jpg" className="profilepic" />
-                    </div>
-                  </Grid>
-                  <Grid item lg={6} xs={12} justifyContent="center" alignItems="center">
-                    <div>
-                      <h2>Name</h2>
-                      <h5>Description</h5>
-                    </div>
-                    <Button variant="contained" color="error">Profile</Button>
-                  </Grid>
+    if (!load) {
+        fetchUsers().then(() => console.log(users))
+        setLoad(true)
+    }
+
+    const countAmount = () => {
+        setCount(count + 1);
+    };
+
+    return (
+        <div>
+            <h1>Explore</h1>
+
+            <div>
+                <div className="bg-image-container">
+                    <img src="./images/explore page background.jpg" className="bg-image"/>
+                </div>
+                <div className="bg-text">
+                    <h1>Search bar</h1>
+                </div>
+            </div>
+
+            <div className="flex-row">
+                <Grid container alignItems="center" style={{width: "90%"}}>
+                    {Array(users.length).fill(0).map((e, i) => (
+                        <Grid item md={4} sm={6} xs={12}>
+                            <div className="card">
+                                <Grid container alignItems="center">
+                                    <Grid item lg={6} xs={12}>
+                                        <div>
+                                            <img src="./images/sample profile pic.jpg" className="profilepic"
+                                                 alt="sample profile picture"/>
+                                            <h5>{users[i].description}</h5>
+                                        </div>
+                                    </Grid>
+                                    <Grid item lg={6} xs={12} justifyContent="center" alignItems="center">
+                                        <div>
+                                            <h2>{users[i].name}</h2>
+                                            <h5>{users[i].username}</h5>
+                                        </div>
+                                        <Button variant="contained" color="error">Profile</Button>
+                                    </Grid>
+                                </Grid>
+                            </div>
+                        </Grid>
+                    ))}
                 </Grid>
-              </div>
-            </Grid>
-          ))}
-        </Grid>
-      </div>
-    </div>
-  );
+            </div>
+        </div>
+    );
 }
 
 export default Explore;
